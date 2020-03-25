8bb8448de4ce04f992c220e3c2d449c357ff77c8
Matt <mbb670@gmail.com>
2020-03-25 06:37:37 -0400

    moving bikes

--- dev/js/anamations/bikes.js
+++ dev/js/anamations/bikes.js
@@ -1,2 +1,24 @@
 // BIKES
 gsap.to(".wheel", {duration:2, rotate:360, transformOrigin:"center", repeat:-1, ease:"linear"});
+
+gsap.to(".back-bike", {motionPath:{
+path: "#Follow-1",
+  align: "#Follow-1",
+  alignOrigin: [.8, .8],
+  autoRotate: 20,
+  start: 1,
+  end: 0},
+
+ duration:10,
+repeat:-1});
+
+gsap.to(".front-bike", {motionPath:{
+path: "#Follow-1",
+  align: "#Follow-1",
+  alignOrigin: [.8, .8],
+  autoRotate: 20,
+  start: .75,
+  end: .25},
+
+ duration:10,
+repeat:-1});
